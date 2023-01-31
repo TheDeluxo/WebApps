d2163af2f50abe9e3bea19e51407d53cbb3d8c1c
Dimitar <55024213+TheDeluxo@users.noreply.github.com>
2023-01-31 16:19:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,7 @@ def index():
         name = "World"
     return render_template("index.html", placeholder=name)  # "placeholder" is the variable in the html file
                                                             # "name" is the variable in the py file
+    
+    
+    if __name__ == "__main__":
+        app.run(debug=false, host='0.0.0.0')
