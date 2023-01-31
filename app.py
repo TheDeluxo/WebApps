21d9b36bc22e52de9ce53796ecbae53b840d7b3f
Dimitar <55024213+TheDeluxo@users.noreply.github.com>
2023-01-31 15:01:11 +0200

    Get rich or die trying

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+# @ is called decorator
+@app.route('/')
+def index():
+    if "name" in request.args:
+        name = request.args["name"]
+    else:
+        name = "World"
+    return render_template("index.html", placeholder=name)  # "placeholder" is the variable in the html file
+                                                            # "name" is the variable in the py file
